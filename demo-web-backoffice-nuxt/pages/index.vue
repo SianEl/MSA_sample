dacd4e0a5b3b87386e2462248d95ca04044d93b6
PP08793 <11st.pp08793@partner.sk.com>
2022-11-18 19:57:41 +0900

    로그인 페이지 생성

--- demo-web-backoffice-nuxt/pages/index.vue
+++ demo-web-backoffice-nuxt/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>메인 페이지</h1>
+    <nuxt-link :to="{path: 'loginForm', query: {prevUrl: '/'}}"><button>로그인 페이지</button></nuxt-link>
 </template>
 <script lang="ts">
 export default {
--- demo-web-backoffice-nuxt/pages/loginForm.vue
+++ demo-web-backoffice-nuxt/pages/loginForm.vue
@@ -1,10 +1,31 @@
 <template>
     <div>
-        로그인 영역
+        <span>ID : </span>
+        <input type="text" id="id" placeholder="ID를 입력하세요"/>
+        <span>PW : </span>
+        <input type="password" id="pw" placeholder="PW를 입력하세요"/>
+        <button @click="login()">로그인</button>
     </div>
 </template>
 <script lang="ts">
 export default {
-    layout: 'default'
+    props: {
+        prevUrl: String
+    },
+    layout: 'default',
+    setup(props, context) {
+        console.log(props.prevUrl)
+    },
+    methods: {
+        login() {
+            let id = document.getElementById("id").value;
+            let pw = document.getElementById("pw").value;
+
+            
+        }
+    },
+    mounted() {
+        
+    }
 }
 </script>
