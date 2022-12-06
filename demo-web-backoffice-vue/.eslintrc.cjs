ce82563041fd2b337531f900d5e32533268889ab
PP08793 <11st.pp08793@partner.sk.com>
2022-12-06 13:48:24 +0900

    csLint 사용

--- demo-web-backoffice-vue/.eslintrc.cjs
+++ demo-web-backoffice-vue/.eslintrc.cjs
@@ -1,14 +1,33 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
   ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: '2022',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
