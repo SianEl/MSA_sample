4112632647b85134520311a39962ccd613e4a32f
PP08793 <11st.pp08793@partner.sk.com>
2022-12-06 12:34:54 +0900

    Vue3 기본 프로젝트 추가

--- demo-web-backoffice-vue/src/main.js
+++ demo-web-backoffice-vue/src/main.js
@@ -1,13 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import AppHeader from "./components/AppHeader.vue";
-import AppNav from "./components/AppNav.vue";
-import AppView from "./components/AppView.vue";
-import BookComponent from "./components/BookComponent.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-app.component("AppNav", AppNav);
-app.component("AppHeader", AppHeader);
-app.component("AppView", AppView);
-app.component("BookComponent", BookComponent);
-app.mount("#app");
+import './assets/main.css'
+
+createApp(App).mount('#app')
