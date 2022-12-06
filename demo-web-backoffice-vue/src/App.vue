437fe469b77aace9c881cbcb1e3a073c15b4c615
PP08793 <11st.pp08793@partner.sk.com>
2022-12-06 16:21:07 +0900

    Composition API에 대해 이해

--- demo-web-backoffice-vue/src/App.vue
+++ demo-web-backoffice-vue/src/App.vue
@@ -1,5 +1,24 @@
-<script setup></script>
-
 <template>
-  <h3>Hello Vue3</h3>
+  <div>
+    <button v-on:click="increment">Counter: {{ counter }}</button>
+  </div>
 </template>
+
+<script>
+import { onMounted, ref } from 'vue';
+export default {
+  setup() {
+    const counter = ref(0);
+    const increment = () => counter.value++;
+    onMounted(() => {
+      console.log('컴포넌트가 마운트 되었습니다!');
+    });
+    return {
+      counter,
+      increment,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
